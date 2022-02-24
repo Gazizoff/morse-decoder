0110a88eea592279270b96d7914074aac1094260
Kanstantsin Hazizau <gazizov.ku@yandex.ru>
2022-02-24 16:50:26 +0300

    feat: another solution

--- src/index.js
+++ src/index.js
@@ -38,38 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    let arr =[]
-    for(let i = 10; i<=expr.length; i+=10){
-        let miniStr = expr.slice(i-10,i)
-        console.log(miniStr)
-        arr.push(miniStr)
-        console.log(arr)
+    // write your solution here
+    let arr = [];
+    let result = '';
+    let i = 0;
+    let j = 10;
+    for (i = 0; i < expr.length;) {
+        arr.push(expr.slice(i, j));
+        i = j;
+        j = j + 10;
     }
-    return arr.map(decodeStr)
-    .map(item => MORSE_TABLE[item] ? MORSE_TABLE[item] : ' ')
-    .join('')
-    function decodeStr(str){
-        let newStr = ''
-        do {
-            if(str[0]+str[1] == '00'){
-                str = str.slice(2,str.length)
+    console.log("array",arr);
+    function decodeElemArr(elem) {
+        if (elem == '**********'){
+            return elem = ' ';
+        }
+        let i = 0;
+        let j = 2;
+        let newElemArr = '';
+        for (i = 0; j <= 10;) {
+            let elemStr = elem.slice(i, j);
+            if (elemStr == '10') {
+                newElemArr = newElemArr + '.';  
+                i = j;
+                j = j + 2;              
             }
-            if(str[0]+str[1] == '10'){
-                newStr +='.'
-                str = str.slice(2,str.length)
+            else if (elemStr == '11') {
+                newElemArr = newElemArr + '-';
+                i = j;
+                j = j + 2;
             }
-            if(str[0]+str[1] == '11'){
-                newStr +='-'
-                str = str.slice(2,str.length)
+            else {
+                i = j;
+                j = j + 2;
             }
-            if(str[0]+str[1] == '**'){
-                newStr +='**'
-                str = str.slice(2,str.length)
-            }
-        }while (str.length>=2)
-        return newStr
+        }
+        return newElemArr;
     }
+    function decoderElemArrToString (elem) {
+        if (MORSE_TABLE[elem] !== undefined) {
+            result += MORSE_TABLE[elem];
+        }
+        else {
+            result += ' ';
+        }
+        return elem;
     }
+    let newArr = arr.map(elem => decodeElemArr(elem));
+    console.log("newArr",newArr);
+    newArr.map(elem => decoderElemArrToString(elem));
+    console.log("result",result);
+    return result;
+}
 module.exports = {
     decode
 }
